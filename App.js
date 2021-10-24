ceb05ef478d8532f85ac437298541fbf70de7835
garjo17 <39463300+garjo17@users.noreply.github.com>
2021-10-24 08:24:58 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,8 +2,29 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     launch: function() {
-        //Write app code here
+        console.log('Hola cara de Bola');
 
-        //API Docs: https://help.rallydev.com/apps/2.1/doc/
+        var myStore = Ext.create('Rally.data.wsapi.Store', {
+            model: 'User Story',
+            autoLoad: true,
+            listeners: {
+                load: function(myStore, myData, success) {
+                    console.log('ya estamos cargado datos....', myStore, myData, success);
+
+                    var myGrid = Ext.create('Rally.ui.grid.Grid', {
+                        store: myStore,
+                        columnCfgs: [
+                            'FormattedID',
+                            'Name',
+                            'Owner'
+                        ]
+
+                    });
+
+                    console.log('my Grid', myGrid);
+                }
+            },
+            fetch: ['FormattedID','Name', 'ScheduleState']
+        });
     }
 });
