30fda628b840b881a0c52a03e7698c8a439f8b1f
garjo17 <39463300+garjo17@users.noreply.github.com>
2021-10-25 06:23:37 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,48 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
+
+    launch: function() {
+        console.log('Hola cara de Bola');
+        this._loadData();
+
+    },
+
+    //Get data from Rally
+    _loadData: function() {
+        var myStore = Ext.create('Rally.data.wsapi.Store', {
+            model: 'User Story',
+            autoLoad: true,
+            listeners: {
+                load: function(myStore, myData, success) {
+                    console.log('ya estamos cargado datos....', myStore, myData, success);
+                    this._loadGrid(myStore);            
+                },
+                scope: this 
+            },
+            fetch: ['FormattedID','Name', 'Owner' ,'ScheduleState']
+        });
+    },
+  // Create a Show Grid
+    _loadGrid: function(myStoryStore) {
+        var myGrid = Ext.create('Rally.ui.grid.Grid', {
+            store: myStoryStore,
+            columnCfgs: [
+                'FormattedID',
+                'Name',
+                'Owner',
+                'ScheduleState'
+            ]
+
+        });
+        console.log('my Grid', myGrid);
+        this.add(myGrid);
+        console.log('my What is this', this);
+    }
+});
+
+
+    /*
     launch: function() {
         console.log('Hola cara de Bola');
 
@@ -32,3 +74,4 @@ Ext.define('CustomApp', {
         });
     }
 });
+*/
